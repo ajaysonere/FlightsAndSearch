2cab315a8b69241ea55c6da075ae30f7baed5321
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-09 23:47:16 +0530

    dotenv

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-a
+const express = require("express");
+require("dotenv").config;
+const setupAndStartServer = async () => {
+    // create express server
+    const app = express();
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is runnig at  http://localhost:${process.env.PORT}`);
+    });
+}
+
+setupAndStartServer();
