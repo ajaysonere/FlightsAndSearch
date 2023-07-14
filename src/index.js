a8ee6392db4c607e27d74e8de2c1c216bd457d05
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-14 22:24:46 +0530

    body parsar added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const {PORT} = require("./config/serverConfig.js");
+
 
 
 const setupAndStartServer = async () => {
     // create express server
+
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    
+
     app.listen(PORT, () => {
         console.log(`Server is runnig at  http://localhost:${PORT}`);
     });
