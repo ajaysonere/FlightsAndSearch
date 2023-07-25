23c26af484310672a6687438906e84f7f8b318e1
Ajay Sonere <ajaysonere786@gmail.com>
2023-07-25 17:11:18 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,24 +1,52 @@
+const city = require('../models/city.js');
 const {city} = require('../models/index.js');
 
 
 class cityRepository{
-     async createCity(name){
+
+     async createCity({name}){
         try{
-           const city = await city.create(name);
+           const city = await city.create({name:name});
            return city;
         }catch(error){
-            throw {error};
+           console.log("Something goes wrong in create city ");
+           throw {error};
         }
      }
 
      async deleteCity(cityId){
         try {
             await city.destory({
-                where: {
+                where : {
                     id : cityId
                 }
-            })
+            });
+            return true;
         } catch (error) {
+            console.log("Somethiing goes wrong in delete city");
+            throw {error};
+        }
+     }
+
+     async updateCity(cityId , name){
+         try{
+            const city = await city.update(name ,{
+                where : {
+                    id : cityId
+                }
+            });
+         }catch(error){
+            console.log("Something goes wrong in update city");
+            throw {error};
+         }
+     }
+
+     async getCity(cityId){
+        try {
+            const city = await city.findByPk(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something goes wrong in get city ");
             throw {error};
         }
      }
